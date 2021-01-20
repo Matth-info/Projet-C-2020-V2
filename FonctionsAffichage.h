09b391791da5313dd4230470ef8e8a2c4e3742c3
Matthias <besnier.matthias@gmail.com>
2021-01-20 12:56:44 +0100

    Create FonctionsAffichage.h

---
+++ FonctionsAffichage.h
@@ -0,0 +1,11 @@
+#ifndef FONCTIONSAFFICHAGE_H
+#define FONCTIONSAFFICHAGE_H
+
+
+void AffichageJeu(ListePerso* Jeu);
+
+void AffichagePlateau(Monde* monde);
+
+void AffichageTresor(int* tresorRouge, int* tresorBleu);
+
+#endif
