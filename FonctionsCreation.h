c3e956419a443d88432bee10b7b829fc44bd191a
Matthias <besnier.matthias@gmail.com>
2021-01-20 12:56:39 +0100

    Create FonctionsCreation.h

---
+++ FonctionsCreation.h
@@ -0,0 +1,18 @@
+#ifndef FONCTIONCREATION_H
+#define FONCTIONCREATION_H
+#include<stdio.h>
+
+ListePerso * initJeu(void);
+
+void CreerChateau(ListePerso* Jeu, Monde* monde, couleur_t couleur,int px, int py);
+
+void CreerSeigneur(ListePerso* Jeu, Monde* monde, couleur_t couleur, int px, int py, int* tresor);
+
+void CreerManant(ListePerso* Jeu,Monde* monde, couleur_t couleur, int px, int py, int* tresor);
+
+void CreerGuerrier(ListePerso* Jeu, Monde* monde, couleur_t couleur, int px, int py, int* tresor);
+
+Case** CreerPlateau(int nbcolonne, int nbligne);
+
+Monde* CreerMonde(Case** plateau);
+#endif
