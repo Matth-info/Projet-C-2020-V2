5ff523e2075af4a13f10de9e8a39b122cee64e54
Matthias <besnier.matthias@gmail.com>
2021-01-20 12:56:48 +0100

    Create FonctionsAction.h

---
+++ FonctionsAction.h
@@ -0,0 +1,12 @@
+#ifndef FONCTIONSACTION_H
+#define FONCTIONSACTION_H
+
+void lancementdePartie(ListePerso* JeuRouge, ListePerso* JeuBleu, Monde *monde, int* tresorRouge, int * tresorBleu);
+void productionManant(Personnage* manant, int* tresor);
+void trahisonManant(Personnage* manant, ListePerso *Attaquant, ListePerso* Perdant);
+void suicide(Personnage* Harakiri,ListePerso *Perdant, Monde* monde);
+void destructionChateau(ListePerso* Attaquant, ListePerso* Perdant, Monde* monde);
+void nouvelleDestination(Personnage* perso,Monde* monde, int newdx, int newdy);
+void deplacementPerso(Personnage* perso, Monde * monde);
+void immobilisation(Personnage* perso);
+#endif
