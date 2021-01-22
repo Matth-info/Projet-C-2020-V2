fb789a7e87b9608e12d3de68a08d99f7881cf4e4
Matthias <besnier.matthias@gmail.com>
2021-01-22 16:09:02 +0100

    Update FonctionsAction.h

--- FonctionsAction.h
+++ FonctionsAction.h
@@ -1,18 +1,18 @@
 #ifndef FONCTIONSACTION_H
 #define FONCTIONSACTION_H
 
-void lancementdePartie(ListePerso* JeuRouge, ListePerso* JeuBleu, Monde *monde, int* tresorRouge, int * tresorBleu);
+void lancementdePartie(ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, Monde *monde, int* tresorRouge, int * tresorBleu);
 void productionManant(Personnage* manant, int* tresor);
-void trahisonManant(Personnage* manant, ListePerso *Attaquant, ListePerso* Perdant);
-void suicide(Personnage* Harakiri,ListePerso *Perdant, Monde* monde);
-void destructionChateau(ListePerso* Attaquant, ListePerso* Perdant, Monde* monde);
+void trahisonManant(Personnage* manant, Personnage*Attaquant, Personnage* Perdant);
+void suicide(Personnage* Harakiri, Monde* monde);
+void destructionChateau(Personnage* ChateauAttaquant, Personnage* ChateauPerdant, Monde* monde, ListePerso* JeuVoisinPerdant);
 void nouvelleDestination(Personnage* perso,Monde* monde, int newdx, int newdy);
 void deplacementPerso(Personnage* perso, Monde * monde);
 void immobilisation(Personnage* perso);
-int* TrouverCaseLibre(Monde* monde, Personnage* chateau, ListePerso* Jeu, int* tresor);
-void TourGuerrier(Personnage * guerrier, ListePerso * Jeu, Monde *monde);
-void TourManant( Personnage* manant, ListePerso* Jeu, Monde * monde, int * tresor);
-void TourSeigneur(Personnage * seigneur, ListePerso * Jeu, Monde *monde);
-void TourChateau(ListePerso*Jeu, Monde* monde, int* tresor);
-void Transformartion(Personnage* Seigneur,ListePerso*Jeu, Monde* monde, int* tresor);
+int* TrouverCaseLibre(Monde* monde, Personnage* chateau, int* tresor);
+void TourGuerrier(Personnage * guerrier, Monde *monde);
+void TourManant( Personnage* manant, Monde * monde, int * tresor);
+void TourSeigneur(Personnage * seigneur, Monde *monde);
+void TourChateau(Personnage * chateau,Monde* monde, int* tresor);
+void Transformartion(Personnage* Seigneur,ListePerso*JeuVoisin, Monde* monde, int* tresor);
 #endif
