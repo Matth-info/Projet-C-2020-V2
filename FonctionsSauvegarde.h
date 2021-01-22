05a52615fa072bb405cabc5d9edbb97708bf9c56
Matthias <besnier.matthias@gmail.com>
2021-01-22 23:04:23 +0100

    Create FonctionsSauvegarde.h

---
+++ FonctionsSauvegarde.h
@@ -0,0 +1,5 @@
+#ifndef FONCTIONSSAUVEGARDE_H
+#define FONCTIONSSAUVEGARDE_H
+
+void sauvergardeJeu(Monde* monde,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, int* tresorR, int* tresorB);
+#endif
