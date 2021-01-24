dd40b83c80f0b221ad8a69695e3a4994a66376b4
Matthias Besnier <besnier.matthias@gmail.com>
2021-01-24 14:48:29 +0100

    Update FonctionsSauvegarde.h

--- FonctionsSauvegarde.h
+++ FonctionsSauvegarde.h
@@ -2,4 +2,10 @@
 #define FONCTIONSSAUVEGARDE_H
 
 void sauvergardeJeu(Monde* monde,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, int* tresorR, int* tresorB, int ProchainAJouer);
+int Chargementpartie(int argc, char* argv[], Monde* monde,ListePerso * JeuRougeVoisin, ListePerso * JeuBleuVoisin,int* tresorR,int* tresorB);
+void CreerManantChargement(Personnage* chateau, Monde* monde,  couleur_t couleur, int px, int py,int dx, int dy, int* tresor);
+void CreerSeigneurChargement(Personnage* chateau, Monde* monde,  couleur_t couleur, int px, int py,int dx, int dy, int* tresor);
+void CreerGuerrierChargement(Personnage* chateau,Monde* monde, couleur_t couleur, int px, int py,int dx, int dy, int * tresor);
+int DemandeSauvegarde(Monde* monde, ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin,int * tresorR, int* tresorB,int ProchainAJouer);
+
 #endif
