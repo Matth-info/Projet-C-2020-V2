cb7d36334a50b382a18acb9e622a0787e2fa1a83
CurtisR11 <69795500+CurtisR11@users.noreply.github.com>
2021-01-27 00:20:51 +0100

    Update FonctionsCombat.h

--- FonctionsCombat.h
+++ FonctionsCombat.h
@@ -1,6 +1,14 @@
 #ifndef FONCTIONSCOMBATS_H
 #define FONCTIONSCOMBATS_H
 
-
-
+void kill(Personnage* perdant, Monde* monde);
+void CombatMemePerso(Personnage* attaquant, Personnage* defenseur, Monde* monde);
+void CombatSeigneurGuerrier(Personnage* attaquant, Personnage* defenseur, Monde* monde);
+void CombatSeigneurManant(Personnage* attaquant, Personnage* defenseur, Monde* monde);
+void CombatGuerrierSeigneur(Personnage* attaquant, Personnage* defenseur, Monde* monde);
+void CombatGuerrierManant(Personnage* attaquant, Personnage* defenseur, Monde* monde);
+void CombatManantSeigneur(Personnage* attaquant, Personnage* defenseur, Monde* monde);
+void CombatManantGuerrier(Personnage* attaquant, Personnage* defenseur, Monde* monde);
+void combat(Personnage* attaquant, Personnage* defenseur, ListePerso* JeuvoisinRouge, ListePerso* Jeuvoisinbleu, Monde* monde);
+void CombatChateau(Personnage* attaquant, Personnage* defenseur, ListePerso* JeuvoisinRouge, ListePerso* JeuvoisinBleu, Monde* monde);
 #endif
