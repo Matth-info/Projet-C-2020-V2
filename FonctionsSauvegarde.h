58c3b9a98547872b3b324e68d2d3939b8134359f
Matthias Besnier <besnier.matthias@gmail.com>
2021-01-27 00:10:32 +0100

    Update FonctionsSauvegarde.h

--- FonctionsSauvegarde.h
+++ FonctionsSauvegarde.h
@@ -7,5 +7,8 @@ void CreerManantChargement(Personnage* chateau, Monde* monde,  couleur_t couleur
 void CreerSeigneurChargement(Personnage* chateau, Monde* monde,  couleur_t couleur, int px, int py,int dx, int dy, int* tresor);
 void CreerGuerrierChargement(Personnage* chateau,Monde* monde, couleur_t couleur, int px, int py,int dx, int dy, int * tresor);
 int DemandeSauvegarde(Monde* monde, ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin,int * tresorR, int* tresorB,int ProchainAJouer);
-
+void traitementFscore(int argc, char** argv, char *NvVainqueur,int nvscore);
+void InsertionNvScore(CoupleNS* tab, int tailletab, CoupleNS nvCoupleNS);
+void triInsertionDecroissant(CoupleNS *tab, int tailletab);
+void Score(int argc, char** argv,Monde* monde,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, int* tresorR, int* tresorB);
 #endif
