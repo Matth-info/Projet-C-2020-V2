201c7d446072db45ced424acf5dcfaf6f75e4564
Matthias Besnier <besnier.matthias@gmail.com>
2021-01-27 00:10:26 +0100

    Update FonctionsSauvegarde.c

--- FonctionsSauvegarde.c
+++ FonctionsSauvegarde.c
@@ -42,7 +42,7 @@ void CreerManantChargement(Personnage* chateau, Monde* monde,  couleur_t couleur
 
 void CreerSeigneurChargement(Personnage* chateau, Monde* monde,  couleur_t couleur, int px, int py,int dx, int dy, int* tresor){ // un seigneur est rataché à un chateau forcément
   Personnage* seigneur=malloc(sizeof(Personnage));
-  if (seigneur==NULL || chateau->typePerso!=Chateau || *tresor-20 < 0 ){
+  if ((seigneur==NULL) || (chateau->typePerso!=Chateau) || (*tresor-20 < 0 )){
       printf("creation du Seigneur impossible\n ");
   }
   else
@@ -224,8 +224,8 @@ int Chargementpartie(int argc, char** argv, Monde* monde,ListePerso * JeuRougeVo
   // 10 pieces d'or dans le coffre Rouge
   //1965968678 pieces d'or dans le coffre Bleu
   int ProchainAJouer=3;
-  if (argc==2){
-    FILE* fichier=fopen(argv[argc-1],"r");
+  if (argv[1]!=NULL){
+    FILE* fichier=fopen(argv[1],"r");
     if (fichier==NULL){
       return 2;
     }else{
@@ -401,4 +401,160 @@ int DemandeSauvegarde(Monde* monde, ListePerso* JeuRougeVoisin, ListePerso* JeuB
         sauvergardeJeu(monde,JeuRougeVoisin,JeuBleuVoisin,tresorR,tresorB,ProchainAJouer);
         return 0;
     }else{return 1;}
+}
+
+void triInsertionDecroissant(CoupleNS *tab, int tailletab)
+{
+    int i,j;
+    CoupleNS tmp;
+    int n=tailletab;
+    for (i=1; i < n; i++)
+    {
+      tmp =tab[i];
+      j=i-1;
+      while ((j>=0) && (tab[j].score < tmp.score))
+      {
+        tab[j+1] = tab[j];
+        j--;
+      }
+      tab[j+1]=tmp;
+    }
+}
+
+void InsertionNvScore(CoupleNS* tab, int tailletab, CoupleNS nvCoupleNS){
+  tab[10].score=nvCoupleNS.score;
+  strcpy(tab[10].nomVainqueur,nvCoupleNS.nomVainqueur);
+  triInsertionDecroissant(tab,11);
+}
+
+void traitementFscore(int argc, char** argv, char *NvVainqueur,int nvscore){
+  CoupleNS nvCoupleNS;
+  strcpy(nvCoupleNS.nomVainqueur,NvVainqueur);
+  nvCoupleNS.score=nvscore;
+  CoupleNS* tab =malloc(11*sizeof(CoupleNS)); // tableau de 10 couples de valeurs initialisé à 0
+  char nv[3]="nv";
+  for (int i=0; i<11; i++){
+    tab[i].score=0;
+    strcpy(tab[i].nomVainqueur,nv);
+  }
+  int m=0; // indice du tableau de CoupleNS
+  char str[T_MAX]="";
+  char nomVainqueur[T_MAX]="";
+  char scoreT[T_MAX]="";
+  int score;
+
+  if (argv[2]!=NULL){
+    FILE* fscore=NULL;
+    fscore=fopen(argv[2],"r+");
+    if (fscore==NULL){
+      printf("erreur lors de l'ouverture du fichier Score\n");
+    }
+    fseek(fscore,0,SEEK_SET); // revenir au début du fichier texte
+    while(fgets(str,T_MAX,fscore)!='\0'){
+        //str est une chaine contenant une ligne : nomVainqueur scoreVainqueur
+        int i;
+        int j;
+        for(i=0; str[i]!=' ';i++){
+          nomVainqueur[i]=str[i];
+        }
+        nomVainqueur[i]='\0'; // nomVainqueur est bien une chaine de caractère;
+        j=++i;
+        while(str[i] != '\0'){
+          scoreT[i-j]=str[i];
+          i++;
+        } scoreT[i-j]='\0'; // score est bien une chaine de caractère
+        score=atoi(scoreT); // récupération de la chaine scoreT pour en récupérer la valeur décimale;
+        strcpy(tab[m].nomVainqueur,nomVainqueur);
+        tab[m].score=score;
+        printf("%s %d\n",tab[m].nomVainqueur, tab[m].score);
+        m++;
+    }
+    // a ce niveau, on a recupere l'integralité des au plus 10 scores dans le tableau tab;
+    // procedons a un tri sur les cles pour être sûr que le prochain score soit bien trié.
+
+    triInsertionDecroissant(tab,11); // m est le nombre de score dans le fichier de score, m+1 après l'ajout de ce score si m<10;
+    // a ce niveau, le tab de couple NS est trié par ordre décroissant
+
+    InsertionNvScore(tab,11,nvCoupleNS);
+
+    // reinscription dans le fichier score des scores actualisé
+    //affichage fichier mis à jour
+    fseek(fscore,0,SEEK_SET); // revenir au début du fichier texte
+    for(int i=0; i<10;i++){
+      printf("%s %d\n",tab[i].nomVainqueur,tab[i].score);
+      fprintf(fscore,"%s %d\n",tab[i].nomVainqueur,tab[i].score);
+    }
+    fclose(fscore);
+    free(tab);
+
+  }else{
+    printf("Pas de fichier score rentre\n");
+  }
+}
+
+
+
+void Score(int argc, char** argv,Monde* monde,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, int* tresorR, int* tresorB){
+  // compter les points de victoire
+  int victorieux;
+  int pointdevictoire = 0;
+  ListePerso* Vainqueur=NULL;
+
+  if (monde->CampRouge==NULL){
+    printf("\nVictoire du joueur Bleu, Felicitation !\n");
+    victorieux=1;
+  }else{
+    printf("\nVictoire du joueur Rouge, Felicitation !\n");
+    victorieux=0;
+  }
+
+
+  if (victorieux==0){
+    Vainqueur=JeuRougeVoisin;
+    pointdevictoire = *tresorR;
+  }else
+  { Vainqueur=JeuBleuVoisin;
+    pointdevictoire = *tresorB;
+  }
+
+  for(Personnage* Chateau=Vainqueur->tete; Chateau!=NULL; Chateau=Chateau->PersoSuivantVoisin){
+      for(Personnage* persotemp=Chateau; persotemp!=NULL; persotemp=persotemp->PersoSuivant){
+        switch(persotemp->typePerso){
+            case 0: printf("un chateau rapporte 30 points\n");
+                      pointdevictoire+=30;
+                      break;
+            case 1: printf("un seigneur rapporte 20 points\n");
+                      pointdevictoire+=20;
+                      break;
+            case 2: printf("un guerrier rapporte 5 points\n");
+                      pointdevictoire+=5;
+                      break;
+            case 3: printf("un manant rapporte 1 points\n");
+                      pointdevictoire+=1;
+                      break;
+      }
+    }
+  }
+  printf("nombre de point de victoire du gagnant : %d\n",pointdevictoire);
+  char nomVainqueur[T_MAX]="";
+  if (argv[2]!=NULL){ // attention les scores doivent etre triés par ordres decroissant, manipulation d'une structure dictionnaire
+    printf("recuperation d'un fichier ScoreParties.got\n");
+    printf("rentrez le nom du Vainqueur: ");
+    scanf("%s",nomVainqueur);
+    printf("ajout de %s %d dans le fichier score\n",nomVainqueur,pointdevictoire);
+    traitementFscore(argc,argv, nomVainqueur,pointdevictoire);
+
+  }else{
+     printf("creation d'un fichier ScoreParties.got \n");
+     FILE* fnewscore=NULL;
+     fnewscore=fopen("ScoreParties.got","w");
+     if (fnewscore==NULL){
+       printf("erreur lors de la creation du fichier score");
+     }
+     printf("rentrez le nom du Vainqueur: ");
+     scanf("%s",nomVainqueur);
+     fprintf(fnewscore,"%s %d",nomVainqueur,pointdevictoire);
+
+     fclose(fnewscore);
+ }
 }
