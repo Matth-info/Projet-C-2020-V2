a2a08a07d1050e74d2ca6dc5e4704118270bd475
Matthias Besnier <besnier.matthias@gmail.com>
2021-01-28 10:10:57 +0100

    Update structure.h

--- structure.h
+++ structure.h
@@ -8,7 +8,7 @@ typedef enum typePerso {Chateau, Seigneur, Guerrier, Manant} typePerso_t;
 typedef enum typeProd {seigneur,guerrier,manant,Rien} typeProd_t;
 typedef enum couleur{Rouge, Bleu} couleur_t;
 
-typedef enum direction{Nord,Sud,Est,Ouest,NordEst, NordOuest, SudEst, SudOuest } direction_t;
+typedef enum direction{Nord,Sud,Est,Ouest,NordEst, NordOuest, SudEst, SudOuest} direction_t;
 
 typedef struct mouvement{
   direction_t direction;
