41f6dd8f8d0225d9a3cdfd4732d7feb63686c36b
Matthias Besnier <besnier.matthias@gmail.com>
2021-01-31 15:38:02 +0100

    Update FonctionsAction.h

--- FonctionsAction.h
+++ FonctionsAction.h
@@ -3,9 +3,9 @@
 
 void lancementdePartie(ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, Monde *monde, int* tresorRouge, int * tresorBleu);
 void productionManant(Personnage* manant, int* tresor);
-void trahisonManant(Personnage* manant, Personnage*Attaquant, Personnage* Perdant);
+void trahisonManant(Personnage* manant, Personnage*Attaquant, Personnage* Perdant,ListePerso* JeuRougeVoisin,ListePerso* JeuBleuVoisin,Monde*monde);
 void suicide(Personnage* Harakiri, Monde* monde);
-void destructionChateau(Personnage* ChateauAttaquant, Personnage* ChateauPerdant, Monde* monde, ListePerso* JeuVoisinPerdant);
+void destructionChateau(Personnage* ChateauAttaquant, Personnage* ChateauPerdant, Monde* monde, ListePerso* JeuVoisinPerdant,ListePerso* JeuVoisinGagnant);
 void nouvelleDestination(Personnage* perso, ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, Monde* monde, int newdx, int newdy);
 void deplacementPerso(Personnage* perso,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, Monde * monde);
 void immobilisation(Personnage* perso,Monde* monde);
@@ -17,7 +17,6 @@ void TourChateau(Personnage * chateau,Monde* monde, int* tresor);
 void Transformation(Personnage* Seigneur,ListePerso*JeuVoisin, Monde* monde, int* tresor);
 void TourDeJeu(Monde* monde,ListePerso* JeuVoisin, ListePerso* JeuVoisinAdverse, int*tresor);
 void moovedir(Personnage* perso,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, Monde * monde, direction_t direction);
-//void depart(Personnage* perso,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, Monde * monde, int px, int py);
 void depart(Personnage* perso,Monde* monde);
 void arrive(Personnage* perso,ListePerso* JeuRougeVoisin, ListePerso* JeuBleuVoisin, Monde * monde, int px, int py);
 #endif
